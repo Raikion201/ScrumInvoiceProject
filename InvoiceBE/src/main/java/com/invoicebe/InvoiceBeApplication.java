0661df45ce439601cafe150cfb6a8bed72ecc11b
HaiTrieuNguyenz <haitrieukt123@gmail.com>
2025-06-17 19:11:28 +0700

    init project

---
+++ InvoiceBE/src/main/java/com/invoicebe/InvoiceBeApplication.java
@@ -0,0 +1,13 @@
+package com.invoicebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceBeApplication.class, args);
+	}
+
+}
