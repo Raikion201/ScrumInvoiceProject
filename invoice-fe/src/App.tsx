3e80c023218666e69ecede4ad73a343870d31e1a
Huy Hoang <hoanghuy140204@gmail.com>
2025-06-18 11:27:25 +0700

    test

--- invoice-fe/src/App.tsx
+++ invoice-fe/src/App.tsx
@@ -1,12 +1,21 @@
 import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'sonner';
 
+import MainLayout from './app/mainlayout'
+import HomePage from './app/page';
 
-
-
-export default function App() {
+const App = () => {
   return (
-
+    <>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </>
   )
-}
+};
+export default App;
