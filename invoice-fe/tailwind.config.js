46c3062d3819ee31ec11ea49aedac14629d74650
Moitapcod <baocongle204@gmail.com>
2025-06-18 14:53:30 +0700

    added blog and connect with backend

--- invoice-fe/tailwind.config.js
+++ invoice-fe/tailwind.config.js
@@ -1,12 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}; 
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ }
