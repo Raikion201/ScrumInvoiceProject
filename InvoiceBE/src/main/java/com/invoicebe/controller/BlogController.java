1d6b2ce1b1e5e4c16a324c638c007f16243ad3e4
QuangHieu1011 <luuquanghieu9.6pht@gmail.com>
2025-06-18 11:35:34 +0700

    Api Blog

---
+++ InvoiceBE/src/main/java/com/invoicebe/controller/BlogController.java
@@ -0,0 +1,57 @@
+package com.invoicebe.controller;
+
+import com.invoicebe.model.Post;
+import com.invoicebe.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/blog/posts")
+public class BlogController {
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post postDetails) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            post.setTitle(postDetails.getTitle());
+            post.setContent(postDetails.getContent());
+            post.setAuthor(postDetails.getAuthor());
+            return ResponseEntity.ok(postRepository.save(post));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        if (postRepository.existsById(id)) {
+            postRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
